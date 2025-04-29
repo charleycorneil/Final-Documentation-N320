5b82810dc3fa320f87699645dfdd67eaab1472ba
Ivy Ditzenberger <emditzen@iu.edu>
2025-04-29 02:46:08 -0400

    forgot to commit. added imgs, created and styled mothman.html, currently working on popups and quiz

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function popquiz() {
+    const answer = prompt("quiz");
+    if (answer !== null) {
+        alert("");
+    }
+}
+
+function comicInfo() {
+    alert("comic");
+  }
+
+  function photoInfo() {
+    alert("photo!");
+  }
