03216046895d6c9843f32076a94c0f0a1a6b46b2
Ty streets <tewalls@iu.edu>
2025-04-29 13:08:56 -0400

    map.html, image cursor added to sytles, map hover effects added to app.js

--- js/app.js
+++ js/app.js
@@ -1,43 +1,54 @@
 function popquiz() {
-    let score = 0;
-    let questionTotal = 3;
+  let score = 0;
+  let questionTotal = 3;
 
-    let question = prompt(`What event is Mothman known for?
+  let question = prompt(`What event is Mothman known for?
     \n 1. The Silver Bridge collapsing
     \n 2. The Texas City disaster
     \n 3. statewide flood across Ohio in 1913`);
-    if (question === "1") {
-        score++;
-    }
+  if (question === "1") {
+    score++;
+  }
 
-    let question2 = prompt(`When was the first Mothman reporting?
+  let question2 = prompt(`When was the first Mothman reporting?
     \n 1. 1966 
     \n 2. 1992 
     \n 3. 1910 `);
-    if (question2 === "1") {
-        score++;
-    }
+  if (question2 === "1") {
+    score++;
+  }
 
-    let question3 = prompt(`What comic does Mothman appear as a villian?
+  let question3 = prompt(`What comic does Mothman appear as a villian?
     \n 1. Superman
     \n 2. Batman
     \n 2. Spiderman`);
-    if (question3 === "2") {
-        score++;
-    }
+  if (question3 === "2") {
+    score++;
+  }
 
-    const percentage = (score / questionTotal) * 100;
-    alert("You got " + percentage + "% correct!");
+  const percentage = (score / questionTotal) * 100;
+  alert("You got " + percentage + "% correct!");
 }
 
 function comicInfo() {
-    alert("comic");
-  }
+  alert("comic");
+}
 
-  function photoInfo() {
-    alert("photo");
-  }
+function photoInfo() {
+  alert("photo");
+}
 
-  function mothInfo() {
-    alert("moth");
-  }
+function mothInfo() {
+  alert("moth");
+}
+
+// Map Hover Effects
+document.querySelectorAll("area").forEach((area) => {
+  area.addEventListener("mouseover", () => {
+    area.style.opacity = "0.7";
+  });
+
+  area.addEventListener("mouseout", () => {
+    area.style.opacity = "1";
+  });
+});
